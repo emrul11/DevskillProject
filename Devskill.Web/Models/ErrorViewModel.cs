612d64b05d918d587dfb9c922932a77b92ce0158
emrul11 <emrulkabir111@gmail.com>
2023-09-12 14:52:41 +0600

    Moved devskill project into git repo

---
+++ Devskill.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Devskill.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
